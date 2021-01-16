e96576784ecab39d5119191cfe0622237ce0a86f
adhil <adhil@example.com>
2021-01-16 13:49:34 +0530

    project created

---
+++ Manager/src/main/java/com/wifiManager/WifiManager.java
@@ -0,0 +1,11 @@
+package com.wifiManager;
+
+import android.app.Activity;
+
+public class WifiManager {
+    Activity activity;
+
+    public WifiManager(Activity activity) {
+        this.activity = activity;
+    }
+}
